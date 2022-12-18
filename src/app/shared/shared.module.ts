5e0d3a0c1193297a91c98cdcbdd805fe60de31f5
Nour-eddine <92918272+F-nour@users.noreply.github.com>
2022-12-18 22:10:54 +0100

    add reusable component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {MatToolbarModule} from "@angular/material/toolbar";
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {CommentsComponent} from './components/comments/comments.component';
+import {MaterialModule} from "./material.module";
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    MatToolbarModule
-  ]
+    declarations: [
+        CommentsComponent
+    ],
+    imports: [
+        CommonModule,
+        MaterialModule,
+        ReactiveFormsModule
+    ],
+    exports: [
+        MaterialModule,
+        CommentsComponent,
+        ReactiveFormsModule
+    ]
 })
-export class SharedModule { }
+export class SharedModule {
+}

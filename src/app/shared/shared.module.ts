344ba76f1f1376a46357d91043897d5c90bd8662
Nour-eddine <92918272+F-nour@users.noreply.github.com>
2023-01-16 06:54:25 +0100

    add pipes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,22 +3,28 @@ import {CommonModule} from '@angular/common';
 import {CommentsComponent} from './components/comments/comments.component';
 import {MaterialModule} from "./material.module";
 import {ReactiveFormsModule} from "@angular/forms";
+import {ShortenPipe} from "./pipes/shorten.pipe";
+import {TimeAgoPipe} from "./pipes/time-ago.pipe";
 
 
 @NgModule({
-    declarations: [
-        CommentsComponent
-    ],
-    imports: [
-        CommonModule,
-        MaterialModule,
-        ReactiveFormsModule
-    ],
-    exports: [
-        MaterialModule,
-        CommentsComponent,
-        ReactiveFormsModule
-    ]
+  declarations: [
+    CommentsComponent,
+    ShortenPipe,
+    TimeAgoPipe
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    MaterialModule,
+    CommentsComponent,
+    ReactiveFormsModule,
+    ShortenPipe,
+    TimeAgoPipe
+  ]
 })
 export class SharedModule {
 }

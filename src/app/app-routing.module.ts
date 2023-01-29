550b2d2b8ef1cab4a4c346d1e0a702ce6c817bf4
Nour-eddine <92918272+F-nour@users.noreply.github.com>
2023-01-29 17:59:01 +0100

    add reactive statement

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ReactiveStateModule} from "./reactive-state/reactive-state.module";
 
 const routes: Routes = [
   { path: 'social-media', loadChildren: () => import('./social-media/social-media.module').then(m => m.SocialMediaModule) },
   { path: 'form', loadChildren: () => import('./form/form.module').then(m => m.FormModule) },
+  { path: 'reactive-state', loadChildren:() => import('./reactive-state/reactive-state.module').then(m => ReactiveStateModule )},
   { path: '**', redirectTo: 'social-media'}
 ];
 
